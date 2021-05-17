ccb6c9e8123038c66252987e3ad2a008e7617667
Bohdan Skakun <bohdan.skakun@eleks.com>
2021-05-17 12:36:56 +0300

    implement controller, index view

---
+++ src/main/java/guru/sprngframwork/spring5jokesappv2/controllers/JokeController.java
@@ -0,0 +1,22 @@
+package guru.sprngframwork.spring5jokesappv2.controllers;
+
+import guru.sprngframwork.spring5jokesappv2.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJokes(Model model){
+        model.addAttribute("joke", jokeService.getJoke());
+        return "index";
+    }
+}
